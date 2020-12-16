b3fe6edc8756d7aa0566119981878a8e6a1816f0
Oğuzhan Çelikarslan <oguzhancelikarslan@gmail.com>
2020-12-16 11:26:48 +0300

    fix indentation error

--- django_rest_framework_base64_fields/fields.py
+++ django_rest_framework_base64_fields/fields.py
@@ -10,28 +10,35 @@ from rest_framework import serializers
 from rest_framework.fields import Field
 
 
+
+
 class Base64FileField(Field):
 
     # mimetypes.guess_extension() may return different values for same mimetype, but we need one extension for one mime
-    _MIME_MAPPING = {
-        'image/jpeg': '.jpg',
-        'audio/wav': '.wav'
-    }
-    _ERROR_MESSAGE = _('Base64 string is incorrect')
+    _MIME_MAPPING = {"image/jpeg": ".jpg", "audio/wav": ".wav"}
+    _ERROR_MESSAGE = _("Base64 string is incorrect")
 
     def to_internal_value(self, data):
         if not isinstance(data, str):
             raise serializers.ValidationError(self._ERROR_MESSAGE)
         try:
-            mime, encoded_data = data.replace('data:', '', 1).split(';base64,')
-            extension = self._MIME_MAPPING[mime] if mime in self._MIME_MAPPING.keys() else mimetypes.guess_extension(mime)
-            file = ContentFile(base64.b64decode(encoded_data), name='{name}{extension}'.format(name=str(uuid.uuid4()),
-                                                                                               extension=extension))
+            mime, encoded_data = data.replace("data:", "", 1).split(";base64,")
+            extension = (
+                self._MIME_MAPPING[mime]
+                if mime in self._MIME_MAPPING.keys()
+                else mimetypes.guess_extension(mime)
+            )
+            file = ContentFile(
+                base64.b64decode(encoded_data),
+                name="{name}{extension}".format(
+                    name=str(uuid.uuid4()), extension=extension
+                ),
+            )
         except (ValueError, binascii.Error):
             raise serializers.ValidationError(self._ERROR_MESSAGE)
         return file
 
-   def to_representation(self, value):
+    def to_representation(self, value):
         if not value:
             return None
         if isinstance(value, ContentFile):
@@ -39,3 +46,4 @@ class Base64FileField(Field):
         else:
             request = self.context.get("request")
             return request.build_absolute_uri(value.url)
+
